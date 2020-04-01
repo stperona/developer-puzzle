7404d7b6ca07d41fc2ff87f2fb0a68ff8e03fb78
sperona <sperona@connectivedx.com>
2020-04-01 15:49:38 -0700

    Adds IEX Cloud sandbox API key for initial project setup.

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
+  apiKey: 'Tpk_74dcc4aa472b41508d57338b81074996',
   apiURL: 'https://sandbox.iexapis.com'
 };
 
