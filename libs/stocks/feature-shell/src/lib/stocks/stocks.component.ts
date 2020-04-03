bd33f3a8369bcb187cfe6fea8b5b9fadd385dd51
sperona <sperona@connectivedx.com>
2020-04-03 16:50:23 -0700

    Adds from and to fields and related validation for custom period range.

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import moment from 'moment';
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -10,25 +11,29 @@ import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-que
 export class StocksComponent implements OnInit {
   stockPickerForm: FormGroup;
   symbol: string;
-  period: string;
+  maxDate = moment().startOf('day');
 
   quotes$ = this.priceQuery.priceQueries$;
-
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required]
+      period: fb.group({
+        from: [null, Validators.required],
+        to: [null, Validators.required]
+      })
+    });
+
+    this.stockPickerForm.get('period').valueChanges.subscribe((value: { from: string, to: string }) => {
+      // If 'from' value is set to before 'to' value set both values to the same date.
+      if (this.stockPickerForm.get('period').valid &&
+          moment(value.to).isBefore(moment(value.from))) {
+        this.stockPickerForm.patchValue({
+          period: {
+            from: value.to
+          }
+        });
+      }
     });
   }
 
